6a9170a67b4c72002a422f07c5a81ecd54ed6ead
RocketGod <jeremy@umcontractors.com>
2023-05-02 10:22:24 -0700

    Update subconvert.py

--- subconvert.py
+++ subconvert.py
@@ -19,24 +19,27 @@ def read_sub_file(filename):
     with open(filename, "r") as file:
         lines = file.readlines()
 
-    frequency = None
     preset = None
     raw_data = []
+    sample_rate = None
+    center_frequency = None
 
     for line in lines:
         if line.startswith("Frequency:"):
-            frequency = int(line.split(":")[1].strip())
+            center_frequency = int(line.split(":")[1].strip())
         elif line.startswith("Preset:"):
             preset = line.split(":")[1].strip()
         elif line.startswith("RAW_Data:"):
             raw_data.extend(map(int, line.split(":")[1].strip().split()))
         elif line.startswith("Key:"):
             raw_data.extend(int(x, 16) for x in line.split(":")[1].strip().split())
+        elif line.startswith("TE:"):
+            sample_rate = int(line.split(":")[1].strip())
 
-    if frequency is None or preset is None or not raw_data:
+    if preset is None or sample_rate is None or center_frequency is None or not raw_data:
         raise ValueError(f"Invalid .sub file format: {filename}")
 
-    return frequency, preset, raw_data
+    return center_frequency, preset, raw_data, sample_rate, center_frequency
 
 def convert_sub_to_signal_file(input_file, output_file, dtype, file_extension=None):
     frequency, preset, raw_data = read_sub_file(input_file)
@@ -99,6 +102,16 @@ def convert_sub_to_complex(raw_data):
     complex_data = np.array([complex(raw_data[i], raw_data[i + 1]) for i in range(0, len(raw_data), 2)], dtype=np.complex64)
     return complex_data
 
+def write_accompanying_txt_file(output_path, frequency, sample_rate=500000):
+    txt_filename = os.path.splitext(output_path)[0] + ".txt"
+    center_frequency = frequency * 1000
+
+    with open(txt_filename, "w") as file:
+        file.write(f"sample_rate={sample_rate}\n")
+        file.write(f"center_frequency={center_frequency}\n")
+
+    print(f"Created accompanying .txt file: {txt_filename}")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Convert a .sub file to a specified signal file format."
@@ -135,6 +148,9 @@ if __name__ == "__main__":
             args.dtype,
             file_extension=args.file_extension,
         )
+        _, _, _, sample_rate, center_frequency = read_sub_file(args.input_file)
+        output_path = os.path.splitext(args.output_file)[0] + SIGNAL_FILE_EXTENSIONS_BY_TYPE[args.dtype][1]
+        write_accompanying_txt_file(output_path, center_frequency, sample_rate)
     except (OSError, ValueError) as e:
         print(f"Error: {e}")
         exit(1)
